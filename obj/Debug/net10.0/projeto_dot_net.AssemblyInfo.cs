9725a855dcd22fc75b78e1f1b9a8e7860e78ec9d
Domwillys <willyanpaproski123@gmail.com>
2025-05-17 00:30:35 -0300

    .

--- obj/Debug/net10.0/projeto_dot_net.AssemblyInfo.cs
+++ obj/Debug/net10.0/projeto_dot_net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto_dot_net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c44a206139923298e7aa9c7675061fca9e3093e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e1f9737d1702f46fe2eafbe0c7b062ca79b8cbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto_dot_net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto_dot_net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
